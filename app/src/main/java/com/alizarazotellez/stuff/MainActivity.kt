fc3bbc644b2a3b757c24b5f2eaf7940ecdd49daa
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2024-06-14 14:29:41 -0500

    Add basic card without hour

--- app/src/main/java/com/alizarazotellez/stuff/MainActivity.kt
+++ app/src/main/java/com/alizarazotellez/stuff/MainActivity.kt
@@ -4,23 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Info
-import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,6 +41,10 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainComponent() {
+    val hour by remember {
+        mutableStateOf("0")
+    }
+
     Scaffold(
         topBar = { TopAppBar(title = { Text(text = "Stuff") }) },
         bottomBar = {
@@ -52,9 +62,19 @@ fun MainComponent() {
         },
     ) { innerPadding ->
         Column(
-            modifier = Modifier.padding(innerPadding)
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(4.dp)
         ) {
-            Text(text = "Hello World!")
+            Card(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(text = "Current hour:", fontSize = 30.sp)
+                    Text(text = hour, fontSize = 25.sp)
+                }
+            }
         }
     }
 }
