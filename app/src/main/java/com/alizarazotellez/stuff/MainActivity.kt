b9ebd1616ffb52457a58d2e339e01bc904df84dc
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2024-06-14 13:44:11 -0500

    Try to learn to use the Scaffold

--- app/src/main/java/com/alizarazotellez/stuff/MainActivity.kt
+++ app/src/main/java/com/alizarazotellez/stuff/MainActivity.kt
@@ -4,44 +4,48 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.alizarazotellez.stuff.ui.theme.StuffTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            StuffTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                            name = "Android",
-                            modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainComponent()
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun MainComponent() {
+    Scaffold(
+        topBar = { TopAppBar(title = { Text(text = "Stuff") }) },
+        bottomBar = {
+            BottomAppBar(actions = {
+                Text(text = "Something...")
+            })
+        },
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            Text(text = "Hello World!")
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-    StuffTheme {
-        Greeting("Android")
-    }
+fun MainComponentPreview() {
+    MainComponent()
 }
